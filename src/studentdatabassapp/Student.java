2884c133df2f577851bb97aba8bc85138a66d1b6
DD <dai@D-10-18-207-5.dhcp4.washington.edu>
2018-10-16 13:40:47 -0700

    build the structure of student database

---
+++ src/studentdatabassapp/Student.java
@@ -0,0 +1,31 @@
+package studentdatabassapp;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int gradeYear;
+    private int studentID;
+    private static int costOfCourse = 600;
+    private int balance;
+    
+    // Constructor: prompt user to enter the name and year for each student
+    public Student(String firstName, String lastName, int year) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gradeYear = year;
+    }
+    
+    //generate a unique ID for each student (first number being their grade level)
+    
+    
+    //enroll in courses
+    
+    
+    //view balance
+    
+    
+    //pay tuition
+    
+    
+    //display student status: name, ID, courses enrolled, and balance
+}
